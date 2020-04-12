19304e1ffc7c92dbbf2a5f998d1beb5b95c71f60
NithiP <the-px@hotmail.com>
2020-04-12 21:58:01 +0700

    covid19 to function

--- app.js
+++ app.js
@@ -39,34 +39,131 @@ app.get('/covid19/xml', (req, res) => {
     })
     .then((response) => {
       const data = response.data.result.records;
-      let xml = '<?xml version="1.0" encoding="UTF-8"?><covid19>';
-      data.forEach((item, i) => {
-        const { no, age, sex, nationality } = item;
-        province_isolation = item['Province of isolation'];
-        notification_date = item['Notification date'];
-        announce_date = item['Announce Date'];
-        province_onset = item['Province of onset'];
-        district_onset = item['District of onset'];
-        xml += `
-          <people>
-            <no>${no}</no>
-            <age>${age}</age>
-            <sex>${sex}</sex>
-            <nationality>${nationality}</nationality>
-            <province_isolation>${province_isolation}</province_isolation>
-            <notification_date>${notification_date}</notification_date>
-            <announce_date>${announce_date}</announce_date>
-            <province_onset>${province_onset}</province_onset>
-            <district_onset>${district_onset}</district_onset>
-          </people>
-        `;
-        connection.query('insert into covid19 set no=?, age=?, sex=?, nationality=?, province_isolation=?, notification_date=?, announce_date=?, province_onset=?, district_onset=?', [no, age, sex, nationality, province_isolation, notification_date, announce_date, province_onset, district_onset], function (error, results, fields) {
-          if (error) throw error;
-        });
-      });
-      xml += '</covid19>';
+      const xml = covid19(data, false);
       const fileName = Math.random().toString(36).substr(2, 10);
-      fs.writeFileSync(`${__dirname}/public/xml/${fileName}.xml`, xml);
-      res.download(`${__dirname}/public/xml/${fileName}.xml`);
+      fs.writeFileSync(`${__dirname}/public/download/xml_covid_${fileName}.xml`, xml);
+      res.download(`${__dirname}/public/download/xml_covid_${fileName}.xml`);
     })
 });
+
+app.get('/covid19/dtd', (req, res) => {
+  axios.get('https://opend.data.go.th/get-ckan/datastore_search?resource_id=93f74e67-6f76-4b25-8f5d-b485083100b6&limit=20', {
+      headers: {
+        'api-key': '2KnKI5JI75np3mY8J5MIsvfaGpwBHnDS'
+      }
+    })
+    .then((response) => {
+      const data = response.data.result.records;
+      const xml = covid19(data, true);
+      const fileName = Math.random().toString(36).substr(2, 10);
+      fs.writeFileSync(`${__dirname}/public/download/dtd_covid_${fileName}.xml`, xml);
+      res.download(`${__dirname}/public/download/dtd_covid_${fileName}.xml`);
+    })
+});
+
+function covid19(data, isDTD){
+    let xml = '<?xml version="1.0" encoding="UTF-8"?><covid19>';
+
+    if(isDTD) xml += `
+    <!DOCTYPE covid19 [
+       <!ELEMENT covid19 [people*] >
+       <!ELEMENT people (no,age,sex,nationality,province_isolation,notification_date,announce_date,province_onset,district_onset)>
+       <!ELEMENT no (#PCDATA)>
+       <!ELEMENT age (#PCDATA)>
+       <!ELEMENT sex (#PCDATA)>
+       <!ELEMENT nationality (#PCDATA)>
+       <!ELEMENT province_isolation (#PCDATA)>
+       <!ELEMENT notification_date (#PCDATA)>
+       <!ELEMENT announce_date (#PCDATA)>
+       <!ELEMENT province_onset (#PCDATA)>
+       <!ELEMENT district_onset (#PCDATA)>
+    ]>`;
+
+    data.forEach((item, i) => {
+      const { no, age, sex, nationality } = item;
+      province_isolation = item['Province of isolation'];
+      notification_date = item['Notification date'];
+      announce_date = item['Announce Date'];
+      province_onset = item['Province of onset'];
+      district_onset = item['District of onset'];
+      xml += `
+        <people>
+          <no>${no}</no>
+          <age>${age}</age>
+          <sex>${sex}</sex>
+          <nationality>${nationality}</nationality>
+          <province_isolation>${province_isolation}</province_isolation>
+          <notification_date>${notification_date}</notification_date>
+          <announce_date>${announce_date}</announce_date>
+          <province_onset>${province_onset}</province_onset>
+          <district_onset>${district_onset}</district_onset>
+        </people>
+      `;
+      connection.query('insert into covid19 set no=?, age=?, sex=?, nationality=?, province_isolation=?, notification_date=?, announce_date=?, province_onset=?, district_onset=?', [no, age, sex, nationality, province_isolation, notification_date, announce_date, province_onset, district_onset], function (error, results, fields) {
+        if (error) throw error;
+      });
+    });
+    xml += '</covid19>';
+    return xml;
+}
+
+app.get('/fish/xml', (req, res) => {
+  axios.get('https://opend.data.go.th/get-ckan/datastore_search?resource_id=ce7f4a78-71db-4754-9084-edca971903bd&limit=20', {
+      headers: {
+        'api-key': '2KnKI5JI75np3mY8J5MIsvfaGpwBHnDS'
+      }
+    })
+    .then((response) => {
+      const data = response.data.result.records;
+      const xml = fish(data, false);
+      const fileName = Math.random().toString(36).substr(2, 10);
+      fs.writeFileSync(`${__dirname}/public/download/xml_covid_${fileName}.xml`, xml);
+      res.download(`${__dirname}/public/download/xml_covid_${fileName}.xml`);
+    })
+});
+
+function fish(data, isDTD){
+    let xml = '<?xml version="1.0" encoding="UTF-8"?><covid19>';
+
+    if(isDTD) xml += `
+    <!DOCTYPE covid19 [
+       <!ELEMENT covid19 [people*] >
+       <!ELEMENT people (no,age,sex,nationality,province_isolation,notification_date,announce_date,province_onset,district_onset)>
+       <!ELEMENT no (#PCDATA)>
+       <!ELEMENT age (#PCDATA)>
+       <!ELEMENT sex (#PCDATA)>
+       <!ELEMENT nationality (#PCDATA)>
+       <!ELEMENT province_isolation (#PCDATA)>
+       <!ELEMENT notification_date (#PCDATA)>
+       <!ELEMENT announce_date (#PCDATA)>
+       <!ELEMENT province_onset (#PCDATA)>
+       <!ELEMENT district_onset (#PCDATA)>
+    ]>`;
+
+    data.forEach((item, i) => {
+      const { no, age, sex, nationality } = item;
+      province_isolation = item['Province of isolation'];
+      notification_date = item['Notification date'];
+      announce_date = item['Announce Date'];
+      province_onset = item['Province of onset'];
+      district_onset = item['District of onset'];
+      xml += `
+        <people>
+          <no>${no}</no>
+          <age>${age}</age>
+          <sex>${sex}</sex>
+          <nationality>${nationality}</nationality>
+          <province_isolation>${province_isolation}</province_isolation>
+          <notification_date>${notification_date}</notification_date>
+          <announce_date>${announce_date}</announce_date>
+          <province_onset>${province_onset}</province_onset>
+          <district_onset>${district_onset}</district_onset>
+        </people>
+      `;
+      connection.query('insert into covid19 set no=?, age=?, sex=?, nationality=?, province_isolation=?, notification_date=?, announce_date=?, province_onset=?, district_onset=?', [no, age, sex, nationality, province_isolation, notification_date, announce_date, province_onset, district_onset], function (error, results, fields) {
+        if (error) throw error;
+      });
+    });
+    xml += '</covid19>';
+    return xml;
+}
